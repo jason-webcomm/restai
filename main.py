dfb5fa50364318ae9118b8bed0eeee1021047d7c
jason <jason@webcomm.com.tw>
2024-04-21 17:46:54 +0800

    pipeline

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     port = int(RESTAI_PORT)
 
     print("Starting server on port " + str(port))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port, loop="asyncio")
