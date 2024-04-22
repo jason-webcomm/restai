36f0cd8aba6da7e944b8b4b0737cf7a7d6a05822
jason <jason@webcomm.com.tw>
2024-04-22 15:22:50 +0800

    pipeline

--- modules/embeddings.py
+++ modules/embeddings.py
@@ -9,4 +9,5 @@ EMBEDDINGS = {
     "google_vertexai": (VertexAIEmbeddings, {}, "public", "https://cloud.google.com/vertex-ai/docs/generative-ai/learn/overview", 1408),
     "all-mpnet-base-v2": (HuggingFaceEmbeddings, {"model_name": "all-mpnet-base-v2"}, "private", "all-mpnet-base-v2 - https://www.sbert.net/docs/pretrained_models.html - https://huggingface.co/sentence-transformers/all-mpnet-base-v2", 768),
     "paraphrase-multilingual-mpnet-base-v2": (HuggingFaceEmbeddings, {"model_name": "paraphrase-multilingual-mpnet-base-v2"}, "private", "paraphrase-multilingual-mpnet-base-v2 - https://www.sbert.net/docs/pretrained_models.html - https://huggingface.co/sentence-transformers/paraphrase-multilingual-mpnet-base-v2", 768),
+    "bge-m3": (HuggingFaceEmbeddings, {"model_name": "BAAI/bge-m3"}, "private", "https://github.com/FlagOpen/FlagEmbedding", 1024),
 }
